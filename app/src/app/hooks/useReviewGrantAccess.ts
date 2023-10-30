fd90e8faa56cc3254ad350df2bf243613b390cd4
Devan <devan.ko@mystenlabs.com>
2023-10-30 17:43:48 +0900

    Add rest hooks.

--- app/src/app/hooks/useReviewGrantAccess.ts
+++ app/src/app/hooks/useReviewGrantAccess.ts
@@ -5,75 +5,64 @@ import { useState } from "react";
 import { useSui } from "./useSui";
 import { TransactionBlock } from "@mysten/sui.js/transactions";
 
-export const useReviewGrantAccess = (
-    review_obj: any ,
-    payment: number,
-    recipient: string ) => {
+export const useReviewGrantAccess = () => {
   const { executeSignedTransactionBlock } = useSui();
   const { signTransactionBlock } = useWalletKit();
-  const [isLoading, setIsLoading] = useState(false);
 
-  const tx = new TransactionBlock();
-  const sendReviewTip = tx.splitCoins(tx.gas, [tx.pure(payment)]);
+  const handleReviewAccessGrant = async(
+    review: string,
+    payment: number,
+    recipient: string,
+    setIsLoading: any
+    ) => {
+      const tx = new TransactionBlock();
+      // split payment amount from balance. 
+      const sendTipAmount = await tx.splitCoins(tx.gas, [tx.pure(payment)]);
+      // start to set up transaction block
+      tx.moveCall({
+        target: `${process.env.NEXT_PUBLIC_PACKAGE_ADDRESS}::review::grant_access_to`,
+        arguments: [
+          tx.object(review),
+          sendTipAmount,
+          tx.pure(recipient),
+        ],
+      });
 
-  tx.moveCall({
-    target: `${process.env.NEXT_PUBLIC_PACKAGE_ADDRESS}::review::grant_access_to`,
-    arguments: [
-      tx.object(review_obj),
-      sendReviewTip,
-      tx.pure(recipient),
-    ],
-  });
-  
-  setIsLoading(true);
-  tx.transferObjects([sendReviewTip], tx.pure(recipient),);
-  console.log("useReviewGrantAccess, signing transaction block...");
-  const signedTx =  signTransactionBlock({
-    transactionBlock: tx,
-  });
-  const resp = executeSignedTransactionBlock({
-    signedTx,
-    requestType: "WaitForLocalExecution",
-    options: {
-      showEffects: true,
-      showEvents: true,
-    },
-  });
-  console.log(resp);
-  return 
-  return signTransactionBlock({
-    transactionBlock: tx,
-  })
-    .then((signedTx: any) => {
-      return executeSignedTransactionBlock({
-        signedTx,
-        requestType: "WaitForLocalExecution",
-        options: {
-          showEffects: true,
-          showEvents: true,
-        },
+      setIsLoading(true);
+      console.log("Grant access, signing transaction block...");
+      return signTransactionBlock({
+        transactionBlock: tx,
       })
-        .then((resp) => {
-          console.log(resp);
-          if (resp.effects?.status.status === "success") {
-            console.log("user granted access to review");
-            toast.success("user granted access to review");
-            return 
-          } else {
-            console.log("user granted access to review failed");
-            toast.error("user granted access to review failed");
-          }
+        .then((signedTx: any) => {
+          return executeSignedTransactionBlock({
+            signedTx,
+            requestType: "WaitForLocalExecution",
+            options: {
+              showEffects: true,
+              showEvents: true,
+            },
+          })
+            .then((resp) => {
+            setIsLoading(false);
+            console.log(resp);
+            if (resp.effects?.status.status === "success") {
+              console.log("Access granted");
+              toast.success("Access granted");
+              return
+            } else {
+              console.log("Access grant failed");
+              toast.error("Access grant failed.");
+              return
+            }
+          })
+            .catch((err) => {
+              setIsLoading(false);
+              console.log("Access grant failed");
+              console.log(err);
+              toast.error("Something went wrong, Access grant failed.");
+          });
         })
-        .catch((err) => {
-          setIsLoading(false);
-          console.log("user granted access to review failed");
-          console.log(err);
-          toast.error("Something went wrong,user granted access to review failed.");
-        });
-    })
-    .catch(() => {
-      setIsLoading(false);
-      console.log("Error while signing tx");
-    });
-};
+    }
+    return { handleReviewAccessGrant }
+}
 
---
+++ app/src/app/hooks/useServicePoeGeneration.ts
@@ -0,0 +1,61 @@
+import { useWalletKit } from "@mysten/wallet-kit"
+import { toast } from "react-hot-toast";
+import { Result } from "../types/Result";
+import { useState } from "react";
+import { useSui } from "./useSui";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+
+export const useServicePoEGeneration = () => {
+    const { executeSignedTransactionBlock } = useSui();
+    const { signTransactionBlock } = useWalletKit();
+
+    const handleServicePoEGeneration = async(
+        setIsLoading: any
+        // need to add arguments here
+    ) => {
+        const tx = new TransactionBlock();
+        tx.moveCall({
+          target: `${process.env.NEXT_PUBLIC_PACKAGE_ADDRESS}::service::generate_proof_of_experience`,
+          arguments: [
+
+            // need to add arguments here
+          ],
+        });
+        setIsLoading(true);
+        console.log("generate proof of experience, signing transaction block...");
+        return signTransactionBlock({
+            transactionBlock: tx,
+            })
+            .then((signedTx: any) => {
+            return executeSignedTransactionBlock({
+                signedTx,
+                requestType: "WaitForLocalExecution",
+                options: {
+                showEffects: true,
+                showEvents: true,
+                },
+            })
+            .then((resp) => {
+                setIsLoading(false);
+                console.log(resp);
+                if (resp.effects?.status.status === "success") {
+                    console.log("Proof of experience generated");
+                    toast.success("Proof of experience generated");
+                    return 
+                } else {
+                    console.log("Proof of experience generation failed");
+                    toast.error("Proof of experience generation failed.");
+                    return
+                }
+            })
+            .catch((err) => {
+                setIsLoading(false);
+                console.log("Proof of experience generation failed");
+                console.log(err);
+                toast.error("Something went wrong, Proof of experience generation failed.");
+                });
+            })
+    }
+    return { handleServicePoEGeneration };
+
+}
