20a683ea4855cb12fd0babda0e55669e6854c93d
Devan <devan.ko@mystenlabs.com>
2023-11-02 16:24:31 +0900

    Add review router.

--- app/src/app/api/review/[id]/route.ts
+++ app/src/app/api/review/[id]/route.ts
@@ -1,17 +1,36 @@
-import {revalidatePath} from "next/cache";
-import {NextRequest, NextResponse} from "next/server";
-import {kv} from "@vercel/kv";
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+import { kv } from "@vercel/kv";
+import { Result } from "@/app/types/Result";
+import { Review } from "@/app/types/Review";
+
+export const fetchCache = "force-no-store";
+export const revalidate = 1;
 
 export const GET = async (request: NextRequest) => {
     const path = request.nextUrl.searchParams.get("path") || "/";
     revalidatePath(path);
 
-    const key = request.nextUrl.pathname.split('/').pop();
-    console.log("key", key);
-    if (!key) {
+    console.log("path", request.nextUrl.pathname);
+    const id = request.nextUrl.pathname.split('/').pop();
+    console.log("id", id);
+    if (!id) {
+        return new NextResponse("Not found", {status: 404});
+    }
+    try {
+        const reviewJson = await kv.get(id.toString());
+        console.log("reviewJson", JSON.stringify(reviewJson));
+        if (!reviewJson) {
+            return new NextResponse("Not found", {status: 404});
+        }
+        return NextResponse.json(
+            reviewJson,
+            { status: 200 }
+        );
+    }catch (error) {
+        console.error(error);
         return new NextResponse("Not found", {status: 404});
     }
 
-    const review = await kv.get<string>(key);
-    return NextResponse.json(review, {status: 200});
+
 };
