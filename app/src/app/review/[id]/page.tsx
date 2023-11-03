0781f0b02e68788065324b050aec219f7fba28c9
Sang Kim <nonvis@gmail.com>
2023-11-03 14:55:14 +0900

    feat: Implement upvote/downvote and lock

--- app/src/app/review/[id]/page.tsx
+++ app/src/app/review/[id]/page.tsx
@@ -2,16 +2,41 @@
 
 import { useParams } from "next/navigation";
 import { useGetReview } from "../../hooks/useGetReview";
-import { useEffect, useState } from "react";
+import { useReviewLocking } from "../../hooks/useReviewLocking";
+import { useReviewVoting } from "../../hooks/useReviewVoting";
+import React, { useState } from "react";
 
 export default function Service() {
   const { id } = useParams();
   const { dataReview, dataReviewBody, isLoading, isError, currentAccount } =
     useGetReview(id);
+  const { handleReviewLocking } = useReviewLocking();
+  const { handleUpvote, handleDownvote } = useReviewVoting();
+  const [isReviewLockLoading, setIsReviewLockLoading] = useState(false);
 
   if (!currentAccount) {
     return <h3>Wallet not connected</h3>;
   }
+
+  const onLockReview = async () => {
+    const res = await handleReviewLocking(id, setIsReviewLockLoading);
+    console.log("lockReview: " + res);
+    window.location.reload();
+  };
+
+  const onUnlockReview = async () => {};
+
+  const onUpvote = async (serviceId: string) => {
+    const res = await handleUpvote(serviceId, id);
+    console.log("upvoteReview: " + res);
+    window.location.reload();
+  };
+
+  const onDownvote = async (serviceId: string) => {
+    const res = await handleDownvote(serviceId, id);
+    console.log("upvoteReview: " + res);
+    window.location.reload();
+  };
 
   const renderContent = () => {
     if (isLoading) {
@@ -20,6 +45,10 @@ export default function Service() {
     if (isError) {
       return <h3>Error</h3>;
     }
+
+    let isOwner = currentAccount?.address === dataReview?.owner;
+    let showLockButton = !dataReview?.is_locked && isOwner;
+    let showUnlockButton = dataReview?.is_locked;
     return (
       <div className="m-5 space-y-3">
         <div>Id: {`${id}`}</div>
@@ -32,9 +61,46 @@ export default function Service() {
         <div>Has POE: {`${dataReview?.has_poe}`}</div>
         <div>Total Score: {`${dataReview?.ts}`}</div>
         <div>Is locked: {`${dataReview?.is_locked}`}</div>
-        <div className="flex flex-row space-x-6 w-screen">
+        <div className="flex flex-row space-x-6">
           <h2 className="self-center">Content</h2>
-          <p className="box-border w-150px p-4 border-4 hover:box-content">{`${dataReviewBody}`}</p>
+          {dataReview?.is_locked && (
+            <p className="w-1/2 box-border p-4 border-4">locked</p>
+          )}
+          {!dataReview?.is_locked && (
+            <p className="w-1/2 box-border p-4 border-4">{`${dataReviewBody}`}</p>
+          )}
+        </div>
+        <div className="flex flex-row space-x-6">
+          {showLockButton && (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-80 rounded"
+              onClick={() => onLockReview()}
+            >
+              Lock Review
+            </button>
+          )}
+          {showUnlockButton && (
+            <button
+              className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 w-80 rounded"
+              onClick={() => onUnlockReview()}
+            >
+              Unlock Review
+            </button>
+          )}
+        </div>
+        <div className="flex flex-row space-x-6">
+          <button
+            className="bg-green-400 hover:bg-gree-700 text-white font-bold py-2 px-4 w-40 rounded"
+            onClick={() => onUpvote(`${dataReview?.service_id}`)}
+          >
+            Up Vote
+          </button>
+          <button
+            className="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 w-40 rounded"
+            onClick={() => onDownvote(`${dataReview?.service_id}`)}
+          >
+            Down Vote
+          </button>
         </div>
       </div>
     );
