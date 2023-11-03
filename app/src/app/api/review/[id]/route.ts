d520ecd6eeef36fd38bcb23fb6d18a958eb6ce1f
Devan <devan.ko@mystenlabs.com>
2023-11-03 15:41:57 +0900

    match with previous commit.

--- app/src/app/api/review/[id]/route.ts
+++ app/src/app/api/review/[id]/route.ts
@@ -2,31 +2,16 @@ import {revalidatePath} from "next/cache";
 import {NextRequest, NextResponse} from "next/server";
 import {kv} from "@vercel/kv";
 
-interface ReviewRequestBody {
-    key: string,
-    value: string
-}
-
-export const POST = async (request: NextRequest) => {
+export const GET = async (request: NextRequest) => {
     const path = request.nextUrl.searchParams.get("path") || "/";
     revalidatePath(path);
 
-    try {
-        const {key, value}: ReviewRequestBody = await request.json();
-        await kv.set(key, value);
-        return NextResponse.json(
-            {
-                success: "Review added",
-            },
-            {status: 200}
-        );
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json(
-            {
-                error: "Failed to add review",
-            },
-            {status: 400}
-        );
+    const key = request.nextUrl.pathname.split('/').pop();
+    console.log("key", key);
+    if (!key) {
+        return new NextResponse("Not found", {status: 404});
     }
+
+    const review = await kv.get<string>(key);
+    return NextResponse.json(review, {status: 200});
 };
